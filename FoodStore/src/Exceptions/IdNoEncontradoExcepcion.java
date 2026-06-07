3bc8e411e190c39160d8115f4b6a9e104aaf1ccc
fernandotupadutn <fernando.weisheim@tupad.utn.edu.ar>
2026-06-07 20:41:34 -0300

    agregado de excepciones y actulizacion de productoService

---
+++ FoodStore/src/Exceptions/IdNoEncontradoExcepcion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author fernando
+ */
+public class IdNoEncontradoExcepcion extends RuntimeException{
+
+    public IdNoEncontradoExcepcion() {
+    }
+
+    public IdNoEncontradoExcepcion(String message) {
+        super(message);
+    }
+
+    public IdNoEncontradoExcepcion(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IdNoEncontradoExcepcion(Throwable cause) {
+        super(cause);
+    }
+    
+    
+    
+}
--- FoodStore/src/integrado/prog2/service/ProductoService.java
+++ FoodStore/src/integrado/prog2/service/ProductoService.java
@@ -4,6 +4,8 @@
  */
 package integrado.prog2.service;
 
+import Exceptions.IdDuplicadoExcepcion;
+import Exceptions.IdNoEncontradoExcepcion;
 import integrado.prog2.entities.Producto;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -14,35 +16,64 @@ import java.util.List;
  * @author fernando
  */
 public class ProductoService {
+
     private List<Producto> productos = new ArrayList<>();
-    
-    public void agregarProducto(Producto nuevoProducto){
-           for (Producto producto : productos) {
-            if(producto.getId().equals(nuevoProducto.getId())){
-                throw new IllegalArgumentException("El producto que quieres agregar ya existe.");
+
+    public void agregarProducto(Producto nuevoProducto) {
+        for (Producto producto : productos) {
+            if (producto.getId().equals(nuevoProducto.getId())) {
+                throw new IdDuplicadoExcepcion("El producto con el ID:" + nuevoProducto.getId() +  "que quieres agregar ya existe.");
             }
         }
         productos.add(nuevoProducto);
     }
-    
-        public void eliminarProducto(Long productoId){
-           Iterator<Producto> iterator = productos.iterator();
 
-           while(iterator.hasNext()){
-               Producto productoAEliminar = iterator.next();
+    public void eliminarProducto(Long productoId) {
+        Iterator<Producto> iterator = productos.iterator();
 
-               if(productoAEliminar.getId().equals(productoId)){
-                   iterator.remove();
-                   return;
-               }
-           }
+        while (iterator.hasNext()) {
+            Producto productoAEliminar = iterator.next();
 
-           throw new IllegalArgumentException("El producto a eliminar no se encontro");
-
+            if (productoAEliminar.getId().equals(productoId)) {
+                productoAEliminar.setDisponible(false);
+                return;
+            }
         }
 
+        throw new IdNoEncontradoExcepcion("El ID: " + productoId + "no se encontro para eliminar el producto.");
+
+    }
+
+    public void editarProducto(Long productoId, Producto producto) {
+        for (Producto productoActualizar : productos) {
+            
+            if (productoActualizar.getId().equals(productoId)) {
+                
+                productoActualizar.setPrecio(producto.getPrecio());
+                productoActualizar.setStock(producto.getStock());
+                productoActualizar.setCategoria(producto.getCategoria());
+                
+                return;
+                
+            }
+            
+        }
+        throw new IdNoEncontradoExcepcion("Error. El ID:" + productoId + "no se encontro." );
+
+    }
     
-   
     
     
+    public List<Producto> listarProductos(){
+        List<Producto> productosDisponibles = new ArrayList<>();
+        for (Producto producto : productos) {
+            if(producto.getDisponible()){
+                productosDisponibles.add(producto);
+            }
+        }
+        
+        
+        return productosDisponibles;
+    }
+
 }
