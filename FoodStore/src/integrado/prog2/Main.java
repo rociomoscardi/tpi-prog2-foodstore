e0d3a7dfcaabf380777d5c79c3d9d58d570cde7a
fernandotupadutn <fernando.weisheim@tupad.utn.edu.ar>
2026-06-13 00:50:43 -0300

    correccion de listado de categorias

--- FoodStore/src/integrado/prog2/Main.java
+++ FoodStore/src/integrado/prog2/Main.java
@@ -13,17 +13,20 @@ public class Main {
         CategoriaService cService = new CategoriaService();
         System.out.println("Sistema Food Store iniciado");
         Categoria c = new Categoria(1l, "postre", "postres frios");
+        Categoria c1 = new Categoria(2l, "cocas", "cocas frios 3l");
         Producto p = new Producto(1l, "helado", 100.00, "palito bombom", 2, "helado.png", true, c);
         Producto p1 = new Producto(2l, "heladoss", 100.00, "palito bombom", 2, "helado.png", true, c);
         
         
        cService.agregarCategoria(c);
+       cService.agregarCategoria(c1);
        pService.agregarProducto(p);
        pService.agregarProducto(p1);
        
-       pService.eliminarProducto(1l);
+       //pService.eliminarProducto(1l);
+       cService.elimiarCategoria(2l);
        
-       cService.editarCategoria(1l, "helados", "palitos bombones");
+       //cService.editarCategoria(1l, "helados", "palitos bombones");
        
        pService.editarProducto(1l, 300, 10, c);
        pService.editarProducto(2l, 300, 10, c);
--- FoodStore/src/integrado/prog2/service/CategoriaService.java
+++ FoodStore/src/integrado/prog2/service/CategoriaService.java
@@ -18,14 +18,21 @@ public class CategoriaService {
     private List<Categoria> categorias = new ArrayList<>();
 
     public List<Categoria> listarCategorias() {
-        return categorias;
-
+        List<Categoria> categoriasDisponibles = new ArrayList<>();
+        for (Categoria categoriaDisponible : categorias) {
+            if (!categoriaDisponible.isEliminado()==true) {
+                categoriasDisponibles.add(categoriaDisponible);
+            }
+          // System.out.println(categoriaDisponible);
+        }
+        return categoriasDisponibles;
     }
 
     public void elimiarCategoria(Long categoriaId) {
         for (Categoria categoria : categorias) {
             if (categoria.getId().equals(categoriaId)) {
                 categoria.setEliminado(true);
+
             }
         }
 
@@ -35,21 +42,21 @@ public class CategoriaService {
 
         for (Categoria categoriaAgregar : categorias) {
             if (categoriaAgregar.getId().equals(categoria.getId())) {
-                throw new IdDuplicadoExcepcion("la categoria ya existe y el ID es: "+ categoria.getId());
+                throw new IdDuplicadoExcepcion("la categoria ya existe y el ID es: " + categoria.getId());
             }
         }
         categorias.add(categoria);
 
     }
 
-    public void editarCategoria(Long categoriaId, String nombre,String descripcion) {
+    public void editarCategoria(Long categoriaId, String nombre, String descripcion) {
         for (Categoria categoriaEditar : categorias) {
-            if(categoriaEditar.getId().equals(categoriaId)){
-                
-                if(categoriaEditar.isEliminado()==true ){
+            if (categoriaEditar.getId().equals(categoriaId)) {
+
+                if (categoriaEditar.isEliminado() == true) {
                     throw new IdEliminadoExecption("El ID:" + categoriaEditar.getId() + "fue removido de la lista y no se puede editar.");
                 }
-                
+
                 categoriaEditar.setNombre(nombre);
                 categoriaEditar.setDescripcion(descripcion);
                 return;
